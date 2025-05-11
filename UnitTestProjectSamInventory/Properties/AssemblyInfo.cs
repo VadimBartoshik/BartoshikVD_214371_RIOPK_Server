04f7c10f1d6d55acc98c094d5714e4f733013c08
Vadim Bartoshik <vadikgreblya@gmail.com>
2025-05-11 23:10:35 +0300

    Добавлены Unit-тесты

---
+++ UnitTestProjectSamInventory/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestProjectSamInventory")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestProjectSamInventory")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("f548bba0-77fa-4d49-808d-0724d6ab1bc6")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnitTestProjectSamInventory/UnitTest1.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OOPIP;
+using System;
+using System.Windows.Forms;
+
+namespace UnitTestProjectSamInventory
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        
+
+        [TestMethod]
+        public void checkSearchTest()
+        {
+            BD bd = new BD();
+            AdminForm adminForm = new AdminForm();
+            Controller controller = new Controller(bd);
+
+            ComboBox comboBox = new ComboBox();
+            comboBox.Text = "2";
+
+            bool expected = true;
+         
+            bool actual = controller.checkSearch(comboBox);
+
+            Assert.AreEqual(expected, actual, "Test is not correct");
+        }
+
+        [TestMethod]
+        public void getSqlCommand()
+        {
+            BD bd = new BD();
+            Controller controller = new Controller(bd);
+
+            string expected = "SELECT id_user, name, login, role FROM user";
+
+            string actual = controller.getSqlCommand("user");
+
+            Assert.AreEqual(expected, actual, "Test is not correct");
+        }
+
+        [TestMethod]
+        public void checkNullgetSqlCommand()
+        {
+            
+            BD bd = new BD();
+            Controller controller = new Controller(bd);
+
+            string expected = "SELECT id_user, name, login, role FROM user";
+            String data = "user";
+            string actual = null;
+            if (checkSearch(data))
+            {
+                 actual = controller.getSqlCommand(data);
+            }
+               
+            Assert.AreEqual(expected, actual, "Test is not correct");
+        }
+
+        public bool checkSearch(String data)
+        {
+            if (data.ToString() == "")
+            {
+                return false;
+            }
+            return true;
+        }
+
+    }
+}
